eb1b906dfc99bea45753a0fe3dd0c0e26d573b30
praveenkumarsamala <praveenkumar.samala@broadcom.com>
2024-07-10 13:01:03 +0530

    updated code

--- .github/unittests.sh
+++ .github/unittests.sh
@@ -4,9 +4,10 @@ set -euo pipefail
 helm plugin install https://github.com/helm-unittest/helm-unittest.git
 
 CHART_DIRS=(gateway portal)
+path=$(pwd)
 
 for CHART_DIR in ${CHART_DIRS[@]}; do
     cd charts/${CHART_DIR}
-    #helm unittest -f 'tests_${CHART_DIR}/*.yaml' .
     helm unittest .
+    cd $path
 done
