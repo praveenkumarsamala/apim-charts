0b6495936bbb32a89a3c412de27816081f200c5f
praveenkumarsamala <praveenkumar.samala@broadcom.com>
2024-07-10 12:44:02 +0530

    updated code

--- .github/unittests.sh
+++ .github/unittests.sh
@@ -3,7 +3,4 @@ set -euo pipefail
 
 helm plugin install https://github.com/helm-unittest/helm-unittest.git
 cd charts/gateway
-helm unittest .
-pwd
-ls -lrt
-hostname
+helm unittest -f 'tests_gateway/*.yaml' .
