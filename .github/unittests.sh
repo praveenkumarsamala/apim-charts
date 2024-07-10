0ae896978da821d1b899e56cbcadb4ee58b26a43
praveenkumarsamala <praveenkumar.samala@broadcom.com>
2024-07-10 12:14:59 +0530

    updated code

--- .github/unittests.sh
+++ .github/unittests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -euo pipefail
 
-sh "helm plugin install https://github.com/helm-unittest/helm-unittest.git > /dev/null 2>&1"
+sh "helm plugin install https://github.com/helm-unittest/helm-unittest.git"
 sh "helm unittest ."
