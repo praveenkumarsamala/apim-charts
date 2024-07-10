c91c58db97cf1a12b7c2f96f274a33bc87d2ff11
praveenkumarsamala <praveenkumar.samala@broadcom.com>
2024-07-10 12:51:32 +0530

    updated code

--- .github/unittests.sh
+++ .github/unittests.sh
@@ -6,6 +6,17 @@ helm plugin install https://github.com/helm-unittest/helm-unittest.git
 CHART_DIRS=(gateway portal)
 
 for CHART_DIR in ${CHART_DIRS[@]}; do
+    echo "step1"
+    pwd
     cd charts/${CHART_DIR}
-    helm unittest -f 'tests_${CHART_DIR}/*.yaml' .
+    #helm unittest -f 'tests_${CHART_DIR}/*.yaml' .
+    helm unittest .
+    echo "step2"
+    pwd
+    cd ..
+    echo "step3"
+    pwd
+    cd ..
+    echo "step4"
+    pwd
 done
